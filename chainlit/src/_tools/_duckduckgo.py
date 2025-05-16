bed21c721a2211502c190c4b72e3c0496911e6b6
isaacanthony <ianthony001@gmail.com>
2025-05-16 18:04:27 -0400

    Add tool docstrings.

--- chainlit/src/_tools/_duckduckgo.py
+++ chainlit/src/_tools/_duckduckgo.py
@@ -6,9 +6,21 @@ from duckduckgo_search import DDGS
 
 @function_tool
 def news(query: str) -> str:
+    """
+    Search DuckDuckGo for current news stories related to a query.
+
+    Args:
+        query: The news query to search DuckDuckGo for.
+    """
     return dumps(DDGS().news(query))
 
 
 @function_tool
 def text(query: str) -> str:
+    """
+    Search DuckDuckGo for pages related to a query.
+
+    Args:
+        query: The query to search DuckDuckGo for.
+    """
     return dumps(DDGS().text(query))
--- chainlit/src/_tools/_wikipedia.py
+++ chainlit/src/_tools/_wikipedia.py
@@ -4,4 +4,10 @@ from wikipedia import summary
 
 @function_tool
 def search(query: str) -> str:
+    """
+    Search Wikipedia for a provided query.
+
+    Args:
+        query: The query to search Wikipedia for.
+    """
     return summary(query)
